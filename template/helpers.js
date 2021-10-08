027298ef66ed1f7cada53cb9af533ce4384392f0
PutraSitorus11S18033 <psitorus270201@gmail.com>
2021-10-08 15:57:31 +0700

    Putra

--- template/helpers.js
+++ template/helpers.js
@@ -1,39 +1,54 @@
 export const getPosts = async () => {
-  // EDIT HERE
-};
+    // EDIT HERE
+    const url = "https://jsonplaceholder.typicode.com/posts?_start=0&_limit=16"
+    let response = await fetch(url)
+    let data = await response.json()
+    return data
+  };
+  
+  
+  export const getPost = async (post_id) => {
+    // EDIT HERE
+    if (post_id !== undefined){
+      getRandomPic()
+      getRandomProfile()
+      getPostComments()
+      getAuthor()
+    } else{
+      console.log('menyerah')
+    }
+  };
+  
+  export const getPostComments = async (post_id) => {
+    // EDIT HERE 
+  };
+  
+  export const getAuthor = async (user_id) => {
+    // EDIT HERE
+  };
+  
+  export const getPostsByAuthor = async (author_id) => {
+    // EDIT HERE
+  };
+  
+  export const getRandomPic = async () => {
+    try {
+      const image = await fetch('https://source.unsplash.com/random/720x480');
+      return image.url;
+    } catch (error) {
+      console.log('getRandomPic', error);
+      throw error;
+    }
+  };
+  
+  export const getRandomProfile = async () => {
+    try {
+      const image = await fetch('https://source.unsplash.com/480x480/?profile');
+      return image.url;
+    } catch (error) {
+      console.log('getRandomProfile', error);
+      throw error;
+    }
+  };
 
-export const getPost = async (post_id) => {
-  // EDIT HERE
-};
-
-export const getPostComments = async (post_id) => {
-  // EDIT HERE
-};
-
-export const getAuthor = async (user_id) => {
-  // EDIT HERE
-};
-
-export const getPostsByAuthor = async (author_id) => {
-  // EDIT HERE
-};
-
-export const getRandomPic = async () => {
-  try {
-    const image = await fetch('https://source.unsplash.com/random/720x480');
-    return image.url;
-  } catch (error) {
-    console.log('getRandomPic', error);
-    throw error;
-  }
-};
-
-export const getRandomProfile = async () => {
-  try {
-    const image = await fetch('https://source.unsplash.com/480x480/?profile');
-    return image.url;
-  } catch (error) {
-    console.log('getRandomProfile', error);
-    throw error;
-  }
-};
+  getPosts();
